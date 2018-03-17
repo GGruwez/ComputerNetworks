ee7ecc7a170f93154e4317d1a05724945297353d
GGruwez <gilles.gruwez@yahoo.com>
2018-03-17 11:34:20 +0100

    .

--- src/main/ServerMain.java
+++ src/main/ServerMain.java
@@ -1,5 +1,12 @@
 package main;
+import inner.*;
 
 public class ServerMain {
 
+	public static void main(String[] args) {
+		Server server = new Server();
+	}
+	
+	public static Server server;
+	
 }
