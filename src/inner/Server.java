8cfb21b99ccb5e7d88112ca0c15b91a6f748a2f7
GGruwez <gilles.gruwez@yahoo.com>
2018-03-17 18:52:01 +0100

    .

--- src/inner/Server.java
+++ src/inner/Server.java
@@ -8,26 +8,43 @@ import java.util.ArrayList;
 
 public class Server {
 
-    public ArrayList<Handler> clients = new ArrayList<>();
     final int portNumber = 1000;
 	
-	public Server() throws Exception{
+	public Server(int portNumber) throws Exception{
 			
+	    	//public ArrayList<Handler> clients = new ArrayList<>();
+		
+			if (portNumber < 0) {
+				throw new IllegalArgumentException("Invalid port");
+			}
+
 			ServerSocket serverSocket = new ServerSocket(portNumber);
 			System.out.println("Waiting for client");
 			
-			
+			/*
+			 *  Accept incoming connections, create thread for each one of them,
+			 *	while listening to more incoming connections
+			 *  As stated in the course multi-threading can be done with thread pools
+		 	 * https://docs.oracle.com/javase/tutorial/essential/concurrency/pools.html
+			 * https://softwareengineering.stackexchange.com/questions/173575/what-is-a-thread-pool
+			 */
 			while (true){	
 				Socket clientSocket = serverSocket.accept();
 				
 				if (clientSocket != null){
 					
 				//clients.add(clientSocket);	
-				Handler h = new Handler(clientSocket);
-                Thread thread = new Thread(h);
+				Handler handler = new Handler(clientSocket);
+                Thread thread = new Thread(handler);
                 thread.start();
 				}
 		
 		}	
 	}
+	
+	public int getPort(){
+		return this.portNumber;
+	}
+	
+	
 }
