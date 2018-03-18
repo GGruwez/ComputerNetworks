aebcc7581513bcaf7d421f75d76eb06769abfc15
GGruwez <gilles.gruwez@yahoo.com>
2018-03-18 11:05:48 +0100

    main

--- src/main/ServerMain.java
+++ src/main/ServerMain.java
@@ -4,7 +4,8 @@ import inner.*;
 public class ServerMain {
 
 	public static void main(String[] args) throws Exception {
-		Server server = new Server();
+		Server server = new Server(1000);
+		new Thread(server).start();
 	}
 	
 	
