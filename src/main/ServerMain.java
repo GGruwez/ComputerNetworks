a1fad499192e1235a1690ac834e6afbe9fa92303
GGruwez <gilles.gruwez@yahoo.com>
2018-03-19 16:37:54 +0100

    .

--- src/main/ServerMain.java
+++ src/main/ServerMain.java
@@ -12,8 +12,8 @@ public class ServerMain {
 	public static void main(String[] args) throws UnknownRequestException, URISyntaxException, UnknownHTTPVersionException {
 		
 		
-		//Server server = new Server(1997);
-		//server.start();
+		Server server = new Server(1997);
+		server.start();
 		
 		Parser.parseRequestHeader("GET www.tinyos.net/ 80 HTTP/1.1");
 		
